2cc8e3e2e1e5ea83182da0c26f1fefc477670858
Napat <napat.charuphant1@gmail.com>
2022-05-05 16:08:15 +0700

    docs: move criteria to readme

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# trilema-scale
-Created with CodeSandbox
+# Trilema-scale
+
+Blockchain trilema in radar chart
+
+# Goal
+
+Due to incresing 
+
+# Evaluation criteria
+
+    // NOTE All of this bias toward existing technology, the upperbound might not be future proof.
+    // TODO we gonna need some kind of ratio or coefficiant here later
+Score range from 1 to 5, anything less than 1 is not permit, to create a centerline at 3.
+
+Scalability analysis using relative comparison with prior data.
+Scalability is define as an ability to increasingly support more node, load, feature, Storage, bandwidth without exponential incuring cost, latency and no significant degredation in performance
+Scalability score at 1 if number of node have a comparably scalability to Bitcoin in 2020
+Scalability score at 3 if number of node in the chain scale linearly with cost
+Scalability score at 5 if the chain scale like BFT permissioned node
+
+QUESTION can we make this a percentage
+Security in term of compromise for scalability and/or decentralization not relate to software quality.
+Security score at 5 if chain require more that 1/2 dishonest node to compromise both consistency and liveness of the network
+Security score at 4 if chain require more that 1/2 dishonest node to compromise consistency or liveness of the network
+Security score at 3 if chain require 1/3 dishonest node to compromise liveness of the network
+Security score at 2 if chain require 1/3 dishonest node to compromise consistency of the network
+Security score at 1 if chain require 1/3 dishonest node to compromise both consistency and liveness of the network
+
+Network Concensus Provider (NCP) is a network participant that influence concensus of the network
+Decentralize score at 1 if 100% NCP interest aligned
+Decentralize score at 3 if 50% of NCP interest aligned
+Decentralize score at 5 if 0% NCP interest aligned
--- src/index.js
+++ src/index.js
@@ -10,28 +10,6 @@ const config = {
   type: "radar",
   data: {
     labels: ["Scalability", "Decentralization", "Security"],
-    // NOTE All of this bias toward existing technology, the upperbound might not be future proof.
-    // TODO we gonna need some kind of ratio or coefficiant here later
-    // Score range from 1 to 5, anything less than 1 is not permit, to create a centerline at 3.
-
-    // Scalability analysis using relative comparison with prior data.
-    // Scalability is define as an ability to increasingly support more node, load, feature, Storage, bandwidth without exponential incuring cost, latency and no significant degredation in performance
-    // Scalability score at 1 if number of node have a comparably scalability to Bitcoin in 2020
-    // Scalability score at 3 if number of node in the chain scale linearly with cost
-    // Scalability score at 5 if the chain scale like BFT permissioned node
-
-    // QUESTION can we make this a percentage
-    // Security interm of thing that get compromise for sake of scalability and/or decentralization
-    // Security score at 5 if chain require more that 1/2 dishonest node to compromise both consistency and liveness of the network
-    // Security score at 4 if chain require more that 1/2 dishonest node to compromise consistency or liveness of the network
-    // Security score at 3 if chain require 1/3 dishonest node to compromise liveness of the network
-    // Security score at 2 if chain require 1/3 dishonest node to compromise consistency of the network
-    // Security score at 1 if chain require 1/3 dishonest node to compromise both consistency and liveness of the network
-
-    // Network Concensus Provider (NCP) is a network participant that influence concensus of the network
-    // Decentralize score at 1 if 100% NCP interest aligned
-    // Decentralize score at 3 if 50% of NCP interest aligned
-    // Decentralize score at 5 if 0% NCP interest aligned
 
     // This is an initial data set subject to debate and change
     datasets: [
@@ -99,7 +77,7 @@ const config = {
         // Ethereum upperleft logo color
         borderColor: "rgb(141, 141, 141, 1.0)",
         pointBackgroundColor: "rgb(141, 141, 141, 1.0)",
-        data: [1, 5, 3]
+        data: [1, 5, 5]
       },
       {
         label: "Stella",
@@ -116,6 +94,14 @@ const config = {
         borderColor: "rgb(44, 86, 221, 1.0)",
         pointBackgroundColor: "rgb(44, 86, 221, 1.0)",
         data: [5, 1, 2.5]
+      },
+      {
+        label: "Libra",
+        backgroundColor: "rgb(44, 86, 221, 0.2)",
+        // Qourum logo color
+        borderColor: "rgb(44, 86, 221, 1.0)",
+        pointBackgroundColor: "rgb(44, 86, 221, 1.0)",
+        data: [4.9, 1, 2.5]
       }
     ]
   },
